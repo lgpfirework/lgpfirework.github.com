afb4a101654853e800abf15441b7cadd0b6d4e53
fireworks <542438121@qq.com>
2015-08-05 16:21:43 +0800

    Signed-off-by: fireworks <542438121@qq.com>

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,13 +1,3 @@
-/* Smooth scrolling
-   Changes links that link to other parts of this page to scroll
-   smoothly to those links rather than jump to them directly, which
-   can be a little disorienting.
-   
-   sil, http://www.kryogenix.org/
-   
-   v1.0 2003-11-11
-   v1.1 2005-06-16 wrap it up in an object
-*/
 
 var ss = {
   fixAllLinks: function() {
